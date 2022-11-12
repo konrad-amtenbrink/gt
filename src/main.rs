3598b5f2ce7e17fc8a3602f9a5d6a951453f2f71
Konrad Amtenbrink <konrad02@gmx.de>
2022-11-12 17:29:20 +0100

    feat: add repo in current in directory

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
 use clipboard::ClipboardContext;
 use clipboard::ClipboardProvider;
 use git2::Repository;
+use std::env::current_dir;
 
 fn main() {
     let mut ctx: ClipboardContext = ClipboardProvider::new().unwrap();
-    let path = "";
+    let path = current_dir().unwrap();
     let repo = Repository::open(path).unwrap();
     let head = repo.head().unwrap();
 
     let reflog = repo.reflog(&head.name().unwrap()).unwrap();
     let last_commit = reflog.get(0).unwrap();
-    let last_commit_msg = &last_commit.message().unwrap();
+    let (_, last_commit_msg) = &last_commit.message().unwrap().split_at(8);
 
     ctx.set_contents(last_commit_msg.to_string().to_owned())
         .unwrap();
